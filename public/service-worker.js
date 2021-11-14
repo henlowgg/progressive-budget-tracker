d72da424f75c2733a2b9a107330e484771fe2f10
Jack Ault <henlowgg@outlook.com>
2021-11-14 17:02:43 -0500

    fixed major issue that was a small bug within html doc not linking the SW file properly, renamed, gtg

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,19 +14,15 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
-    caches
-      .open(DATA_CACHE_NAME)
-      .then((cache) => cache.addAll("/api/transaction"))
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
   );
-
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function (evt) {
+self.addEventListener("activate", (evt) => {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
@@ -39,24 +35,34 @@ self.addEventListener("activate", function (evt) {
       );
     })
   );
+
   self.clients.claim();
 });
 
-// fetch event
-
+// fetch
 self.addEventListener("fetch", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
         })
-      );
-    })
-  );
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
 
   evt.respondWith(
     caches.open(CACHE_NAME).then((cache) => {
