3d576ce6015b54aa05f4b678cffd9353be14cb7d
Jack Ault <henlowgg@outlook.com>
2021-11-14 15:30:51 -0500

    getting db prepped for connection to budget db

---
+++ public/db.js
@@ -0,0 +1,11 @@
+let db;
+
+const request = indexedDB.open('BudgetDB', 1);
+
+request.onupgradeneeded = function(event) {
+  db = event.target.result;
+//   may need to remove !
+  if (!db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
+};
