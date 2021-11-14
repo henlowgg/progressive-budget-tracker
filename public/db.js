3683d97fd052c07884e4113b3c4cd8ee5dad3ca3
Jack Ault <henlowgg@outlook.com>
2021-11-14 15:36:10 -0500

    wrote out post routes for the budgetstore and clear

--- public/db.js
+++ public/db.js
@@ -1,15 +1,40 @@
 let db;
 
-const request = indexedDB.open('BudgetDB', 1);
+const request = indexedDB.open("BudgetDB", 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
   db = event.target.result;
-//   may need to remove !
+  //   may need to remove !
   if (!db.objectStoreNames.length === 0) {
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
   }
 };
 
-request.onerror = function(event) {
-    console.log(evt.target.errorCode);
+request.onerror = function (event) {
+  console.log(evt.target.errorCode);
 };
+
+function checkDB() {
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["BudgetStore"], "readwrite");
+          const store = transaction.objectStore("BudgetStore");
+          store.clear();
+        });
+    }
+  };
+}
